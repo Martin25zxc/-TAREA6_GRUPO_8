f17ab4614a7f6f8cd181bd2a3cc61018ce8b8b37
j Martin Povis <martin25zxc@gmail.com>
2021-05-30 14:58:16 -0300

    Se realizaron cambios en PersonaNegocioImpl

--- src/negocioImpl/PersonaNegocioImpl.java
+++ src/negocioImpl/PersonaNegocioImpl.java
@@ -11,30 +11,29 @@ public class PersonaNegocioImpl implements PersonaNegocio{
 	
 	PersonaDao personasDao = new PersonaDaoImpl();
 
-	@Override
 	public Persona get(String dni) {
 		return personasDao.get(dni);
 	}
 
-	@Override
 	public List<Persona> get() {
 		return personasDao.get();
 	}
 
-	@Override
 	public void post(Persona persona) throws PersonaException {
 		String validaciones = "";
 		if(persona == null)
 			validaciones += "El registro de personas se encuentra vacio.";
-		if(persona.getApellido().isEmpty() )
-			validaciones += "Se debe completar el campo apellido.";
-		if(persona.getNombre().isEmpty() )
-			validaciones += "Se debe completar el campo nombre.";
-		if(persona.getDni().isEmpty() ) {
-			validaciones += "Se debe completar el campo dni.";
-		}
-		else if (personasDao.get(persona.dni) != null){
-				validaciones += "Ya se encuentra una persona registrada con ese dni.";
+		else {
+			if(persona.getApellido().isEmpty() )
+				validaciones += "Se debe completar el campo apellido.";
+			if(persona.getNombre().isEmpty() )
+				validaciones += "Se debe completar el campo nombre.";
+			if(persona.getDni().isEmpty() ) {
+				validaciones += "Se debe completar el campo dni.";
+			}
+			else if (personasDao.get(persona.dni) != null){
+					validaciones += "Ya se encuentra una persona registrada con ese dni.";
+			}
 		}
 		
 		if(!validaciones.isEmpty())
@@ -46,15 +45,16 @@ public class PersonaNegocioImpl implements PersonaNegocio{
 			throw new PersonaException("No se pudo guardar el registro de Persona.");
 	}
 
-	@Override
 	public void update(Persona persona) throws PersonaException {
 		String validaciones = "";
 		if(persona == null)
 			validaciones += "El registro de personas se encuentra vacio.";
-		if(persona.getApellido().isEmpty() )
-			validaciones += "Se debe completar el campo apellido.";
-		if(persona.getNombre().isEmpty() )
-			validaciones += "Se debe completar el campo nombre.";
+		else {
+			if(persona.getApellido().isEmpty() )
+				validaciones += "Se debe completar el campo apellido.";
+			if(persona.getNombre().isEmpty() )
+				validaciones += "Se debe completar el campo nombre.";
+		}
 		
 		if(!validaciones.isEmpty())
 			throw new PersonaException(validaciones);
@@ -69,7 +69,7 @@ public class PersonaNegocioImpl implements PersonaNegocio{
 	public void delete(String dni) throws PersonaException {
 		String validaciones = "";
 		if (personasDao.get(dni) == null)
-			validaciones += "Ya se encuentra una persona registrada con ese dni.";
+			validaciones += "No se encontro el registro de persona que se desea eliminar.";
 		
 		if(!validaciones.isEmpty())
 			throw new PersonaException(validaciones);
@@ -78,7 +78,6 @@ public class PersonaNegocioImpl implements PersonaNegocio{
 		
 		if(!isSuccess)
 			throw new PersonaException("No se pudo guardar el registro de Persona.");
-		
 	}
 
 }
