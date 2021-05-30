8bc5e519069be3c3b1a33faca20c6c8cda610cab
j Martin Povis <martin25zxc@gmail.com>
2021-05-30 15:11:57 -0300

    Se quito la referencia a negocioImpl de la interfaz PersonaNegocio

--- src/negocio/PersonaNegocio.java
+++ src/negocio/PersonaNegocio.java
@@ -2,12 +2,11 @@ package negocio;
 import java.util.List;
 
 import entidades.Persona;
-import negocioImpl.PersonaException;
 
 public interface PersonaNegocio {
-	public void post (Persona persona) throws PersonaException;
-	public void update (Persona persona) throws PersonaException;
-	public void delete(String dni) throws PersonaException;
+	public void post (Persona persona) throws Exception;
+	public void update (Persona persona) throws Exception;
+	public void delete(String dni) throws Exception;
 	public Persona get(String dni);
 	public List<Persona> get();
 }
